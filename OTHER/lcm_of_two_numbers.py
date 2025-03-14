019922ab476f4c5b2306bda626131c73becfd4c0
maanya145 <maanyapatel145@gmail.com>
2025-03-14 19:26:07 +0530

    Update lcm_of_two_numbers.py

--- OTHER/lcm_of_two_numbers.py
+++ OTHER/lcm_of_two_numbers.py
@@ -1,4 +1,4 @@
-#1. Write a Python program to find the LCM (Least Common Multiple) of two numbers 
+#1. Write a Python program to find the LCM (Least Common Multiple) of two numbers.
 
 # Taking input from the user
 num1 = int(input("Enter first number: "))
