7944c23f1114af141e23bf10f9a66569d0f92ed8
Maanya Patel <maanyapatel145@gmail.com>
2025-04-08 19:45:03 +0530

    Update program_3_3.py

--- TB_PROGRAMS/ch3/program_3_3.py
+++ TB_PROGRAMS/ch3/program_3_3.py
@@ -1,20 +1,15 @@
 # Program 3.3
-# Write a program that receives two numbers in a function and returns the results 
-# of all arithmetic operations (+, -, *, /, %).
+# Program that receives two numbers in a function and returns the results of all arithmetic operations (+, -, *, /, %) on these numbers.
 
 def arCalc(x, y):
     return x + y, x - y, x * y, x / y, x % y
 
-# Main program
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
-
-# Calling the function and unpacking results
+#__main__
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
 add, sub, mult, div, mod = arCalc(num1, num2)
-
-# Displaying results
-print("Sum of given numbers:", add)
-print("Subtraction of given numbers:", sub)
-print("Product of given numbers:", mult)
-print("Division of given numbers:", div)
-print("Modulo of given numbers:", mod)
+print("Sum of given numbers :", add)
+print("Subtraction of given numbers :", sub)
+print("Product of given numbers :", mult)
+print("Division of given numbers :", div)
+print("Modulo of given numbers :", mod)
