1c68469d469b97c06cef784f9c118d67bd42be9a
maanya145 <maanyapatel145@gmail.com>
2025-04-29 09:53:29 +0530

    class

---
+++ OTHER/6.py
@@ -0,0 +1,11 @@
+#WAP to ensure that integer is given as input in case other, it should display not a valid integer
+
+
+
+try:
+    x = int(input("What's x: "))
+except ValueError:
+    print("Not a valid integer.")
+finally:
+    print(f"x is {x}")
+
