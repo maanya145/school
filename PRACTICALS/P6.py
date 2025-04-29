6279f52a4f11b3cb2e1a2b2e83b51e3842db367d
maanya145 <maanyapatel145@gmail.com>
2025-04-29 09:53:27 +0530

    exceptions

---
+++ PRACTICALS/P6.py
@@ -0,0 +1,8 @@
+#in a school fest, three randomly chosen students out of 100 students...
+
+import random
+student1 = random.randint(1,100)
+student2 = random.randint(1,100)
+student3 = random.randint(1,100)
+print("3 chosen students are",)
+print(student1,student2,student3)
